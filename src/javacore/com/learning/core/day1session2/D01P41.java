5ffb2bc058911d388fc848214eae98c627cf4c9e
BhartiTomar <96335537+BhartiTomar@users.noreply.github.com>
2024-04-04 14:06:46 +0530

    Add files via upload

---
+++ src/javacore/com/learning/core/day1session2/D01P41.java
@@ -0,0 +1,35 @@
+package javacore.com.learning.core.day1session2;
+
+import java.util.Scanner;
+
+public class D01P41 {
+    public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter two numbers in the range 1 to 40 separated by space: ");
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+
+        int[] array = {7, 25, 5, 19, 30};
+
+        boolean foundNumber1 = false;
+        boolean foundNumber2 = false;
+ 
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == number1) {
+                foundNumber1 = true;
+            }
+            if (array[i] == number2) {
+                foundNumber2 = true;
+            }
+        }
+
+        if (foundNumber1 && foundNumber2) {
+            System.out.println("Its Bingo");
+        } else {
+            System.out.println("Not Found Its Bingo");
+        }
+ 
+        scanner.close();
+    }
+}
